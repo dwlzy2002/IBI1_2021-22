bfe71171909425bc1b196a4d4f957427b97464b7
U-DUWEILUN\DUWL <weilun.21@intl.zju.edu.cn>
2022-03-15 20:37:49 +0800

    add comment pizza

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,6 +1,10 @@
 n=0
-while n<64:
-    n = n+1
-    p = (n**2+n+2)/2
-    print("if we cut ",n," times","we will get ",p," slices of pizza.")
+while n<64: 
+    #need to repeat 64 times before everyone can eat a slice of pizza.
+    n = n+1 
+    #add one cut each time before reaching 64.
+    p = (n**2+n+2)/2 
+    #give the computer fomula to calculate.
+    print("if we cut",n,"times","we will get",p,"slices of pizza.") 
+    #print as whole sentences with the numbers.
 
