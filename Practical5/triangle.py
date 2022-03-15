54a1e288ac2059fef46e8eaca8945deae3598619
dwlzy2002 <101240024+dwlzy2002@users.noreply.github.com>
2022-03-15 20:50:53 +0800

    Update triangle.py

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,6 +1,6 @@
 a = 0
 for i in range (1,11): 
-# to set for the range from 1 to 10. 
+# to set for the range from 1 to 10 (both 1 and 10 are included). 
 #if i<=10, calculate and print a. if not, end the loop.
     a = a+i            
 # add the number from 1 to 10
