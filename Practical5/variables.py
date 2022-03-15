cf07c919c3296186683d32067b164d419cd0106b
U-DUWEILUN\DUWL <weilun.21@intl.zju.edu.cn>
2022-03-15 20:22:09 +0800

    correct variables

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,10 +3,11 @@ b = 4218520
 c = 271
 d = b-c
 e = a-b
-print("d = ",d,"e = ",e) # d<e , the rate of 2021 is greater.
+print("d =",d,"e =",e) # d<e , the rate of 2021 is greater.
 
 
-X = "Ture"
-Y = "False"
+
+X = str(input("X:"))
+Y = str(input("Y:"))
 W = X and Y
 print(W)
