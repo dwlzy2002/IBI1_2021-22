3149a32569a775ee48f1af58a614d07f865c2e4d
dwlzy2002 <101240024+dwlzy2002@users.noreply.github.com>
2022-03-18 17:07:49 +0800

    Update pizza_party.py

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,10 +1,15 @@
+#need to find out how many times we need to cut to ensure that everyone can eat a slice of pizza.
 n=0
-while n<64: 
-    #need to repeat 64 times before everyone can eat a slice of pizza.
+p = (n**2+n+2)/2 
+while p < 64 : 
     n = n+1 
-    #add one cut each time before reaching 64.
+    #add one cut each time before reaching 64 slice of pizza.
     p = (n**2+n+2)/2 
-    #give the computer fomula to calculate.
-    print("if we cut",n,"times","we will get",p,"slices of pizza.") 
-    #print as whole sentences with the numbers.
-
+    #give the computer fomula to calculate p.
+    if p < 64:
+        print("if we cut",n,"times","we will get",p,"slices of pizza.","Not enough. Please go on.XD") 
+    else:
+        print("if we cut",n,"times","we will get",p,"slices of pizza.","That's enough. Let's eat.:)")
+    #check the value of p. (because i use the while loop,after 10 times, it will start a new try.I need to check whether p<64 this time to agjust my output sentence.)
+    #p<64,print the sentence and go on adding cut times.
+    #p=64 stop adding and print the"that's enough." sentence.           
