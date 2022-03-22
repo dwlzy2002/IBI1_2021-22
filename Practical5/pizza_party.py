b38892559cc9e325d511ec356381d8c175d06e61
dwlzy2002 <101240024+dwlzy2002@users.noreply.github.com>
2022-03-22 09:15:17 +0800

    Update pizza_party.py

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -2,15 +2,17 @@
 n=0
 p = (n**2+n+2)/2 
 while p < 64 : 
-    n = n+1 
     #add one cut each time before reaching 64 slice of pizza.
-    p = (n**2+n+2)/2 
+    n = n+1 
     #give the computer fomula to calculate p.
-    if p < 64:
-        print("if we cut",n,"times, we will get",p,"slices of pizza.","Not enough. Please go on.XD") 
-    else:
-        print("if we cut",n,"times, we will get",p,"slices of pizza.","That's enough. Let's eat.:)")
+    p = (n**2+n+2)/2 
     #check the value of p and print the sentences.
     #for p<64, print the "not enough." sentence.
     #because i use the while loop,after the last p that is smaller than 64, it will start a new try.so I need to adjust my output sentence for this new p that is equal or bigger than 64.
     #for p>=64, print the"that's enough." sentence.           
+
+    if p < 64:
+        print("if we cut",n,"times, we will get",p,"slices of pizza.","Not enough. Please go on.XD") 
+    else:
+        print("if we cut",n,"times, we will get",p,"slices of pizza.","That's enough. Let's eat.:)")
+    
