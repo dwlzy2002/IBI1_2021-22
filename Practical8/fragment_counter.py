93c2cb446804d9786f26adfa2ef7dbd4e298d29f
dwlzy2002 <101240024+dwlzy2002@users.noreply.github.com>
2022-04-13 17:29:41 +0800

    change into "GAATTC"

--- Practical8/fragment_counter.py
+++ Practical8/fragment_counter.py
@@ -1,5 +1,5 @@
 import re
 seq = 'ATGCAATCGACTACGATCAATCGAGGGCC'
-cut_point = re.findall('GA',seq)
+cut_point = re.findall('GAATTC',seq)
 number = len(cut_point)+1
 print('The total number of fragments is:', number) 
