460db57420f2c1dfd3664e73d0b79aefe4e71b9d
dwlzy2002 <101240024+dwlzy2002@users.noreply.github.com>
2022-04-15 17:04:58 +0800

    Update count_cut_genes.py

--- Practical8/count_cut_genes.py
+++ Practical8/count_cut_genes.py
@@ -1,20 +1,25 @@
 import re
 file_name=input('Enter the file name:')
 f3=open(file_name,'w')
-f2=open('cut_genes.fa')
+f1=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
+
 seq={}
-for line in f2:
+for line in f1:
+    line=line.rstrip()
     if line.startswith('>'):
-        name=re.search(r'(>.+?)\d+',line).group(1)
-        seq[name]=''
+        a=re.search(r'gene:(.+?\s)', line)
+        name="\n"+'>'+a.group(1)
+        seq[name]=""
     else:
         seq[name]+=line
-f2.close()
+f1.close()
+
 for n in seq.keys():
-    cut_point = re.findall('GAATTC',seq[n])
-    number = str(len(cut_point)+1)
-    name_and_number=n+"        "+number
-    name_and_number=name_and_number.strip()
-    f3.write(name_and_number+"\n"+seq[n])
-f2.close()
+    if re.search('GAATTC',seq[n]):
+        cut_point = re.findall('GAATTC',seq[n])
+        number = str(len(cut_point)+1)
+        name_and_number=n+"        "+number
+        name_and_number=name_and_number.strip()
+        f3.write(name_and_number+"\n"+seq[n]+'\n')
+f1.close()
 f3.close()
