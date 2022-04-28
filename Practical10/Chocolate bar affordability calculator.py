9be533818dfaef54859baea46ae68cbd84cee777
dwlzy2002 <101240024+dwlzy2002@users.noreply.github.com>
2022-04-28 19:58:22 +0800

    Delete Chocolate bar affordability calculator.py

--- Practical10/Chocolate bar affordability calculator.py
+++
@@ -1,8 +0,0 @@
-def chocolate(total_money,price):
-    number=total_money//price
-    change=total_money%price
-    print(number,change)
-    return number,change
-total_money=200
-price=7
-chocolate(total_money,price)
