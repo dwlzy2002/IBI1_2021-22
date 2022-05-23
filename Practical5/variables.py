ff2b54cd8dc1719e5356690e20c5592b42b2070a
dwlzy2002 <101240024+dwlzy2002@users.noreply.github.com>
2022-05-23 23:12:33 +0800

    change x and y

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -12,9 +12,19 @@ else:
   print('The rate of 2020 and 2021 are equal.')
 # d<e , the rate of 2021 is greater.
 
-
-
 X = True
 Y = False
+W = X and Y
+print(W)
+X = False
+Y = False
+W = X and Y
+print(W)
+X = True
+Y = True
+W = X and Y
+print(W)
+X = False
+Y = True
 W = X and Y
 print(W)
