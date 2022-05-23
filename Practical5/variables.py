10d07fa999960bf74ab453a18c805ac2bc542f8e
dwlzy2002 <101240024+dwlzy2002@users.noreply.github.com>
2022-05-23 22:59:30 +0800

    add comparing

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,7 +3,14 @@ b = 4218520
 c = 271
 d = b-c
 e = a-b
-print("d =",d,"e =",e) # d<e , the rate of 2021 is greater.
+print("d =",d,"e =",e)
+if d<e:
+  print('The rate of 2021 is greater.')
+elif d>e:
+  print('The rate of 2020 is greater.')
+else:
+  print('The rate of 2020 and 2021 are equal.')
+# d<e , the rate of 2021 is greater.
 
 
 
