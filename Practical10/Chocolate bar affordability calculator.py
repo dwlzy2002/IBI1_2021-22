136e860f237c9c38d9601f56380d11219458fb62
dwlzy2002 <101240024+dwlzy2002@users.noreply.github.com>
2022-05-24 22:17:09 +0800

    add explanation

--- Practical10/Chocolate bar affordability calculator.py
+++ Practical10/Chocolate bar affordability calculator.py
@@ -3,6 +3,7 @@ def chocolate(total_money,price):
     change=total_money%price
     print("We can buy how many chocolate:",number,"The change left:",change)
     return number,change
+#example:
 total_money=200
 price=7
 chocolate(total_money,price)
